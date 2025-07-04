aa731b2863b8c2cbdd19bc2640dffebe8d3cd78c
Govind <govind01012003@gmail.com>
2025-07-04 11:41:42 +0530

    chatbot ui updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+// App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -7,31 +8,29 @@ import ChatbotPage from "./pages/ChatbotPage";
 import Login from "./pages/AuthPages/Login";
 import Signup from "./pages/AuthPages/Signup";
 import ResetPassword from "./pages/AuthPages/ResetPassword";
-import NotFound from './pages/NotFound.jsx';
-import { AuthProvider } from './context/AuthContext.jsx';
-import { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast"; 
+import { useState, useEffect } from "react";
+import Chatbot from "./components/Chatbot";
+
 
 export default function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <div className="min-h-screen flex flex-col bg-gray-50">
-          <Navbar />
-          <Toaster position="top-center" /> 
-          <main className="flex-grow pt-6 pb-12 px-4">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/upload" element={<Upload />} />
-              <Route path="/chatbot" element={<ChatbotPage />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} /> 
-              <Route path="/reset-password" element={<ResetPassword />} />
-              <Route path="*" element={<NotFound />}/>
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <div className="min-h-screen flex flex-col bg-gray-50">
+        <Navbar />
+        <Toaster position="top-right" /> 
+        <main className="flex-grow pt-6 pb-12 px-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/upload" element={<Upload />} />
+            <Route path="/chatbot" element={<ChatbotPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/Signup" element={<Signup />} /> 
+            <Route path="/reset-password" element={<ResetPassword />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
